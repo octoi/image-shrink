aaf2355e06e0393eaf5a5867626a082c45edb077
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-14 13:12:37 +0530

    env setup

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const { BrowserWindow, app } = require('electron');
 
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV == 'development';
+
 let mainWindow;
 
-const createMainWindows = () => {
+const createMainWindow = () => {
     mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         width: 500,
         height: 600,
+        icon: './app/assets/icons/Icon_256x256.png',
+        resizable: isDev, // enable resize option on development mode
     });
 
     mainWindow.loadFile('./app/index.html');
 }
 
-app.on('ready', createMainWindows);
+app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length == 0) {
+        createMainWindow();
+    }
+});
