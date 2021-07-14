f081f8e4e569e3b18f35ef4e8b6432d973e49dc6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-14 12:48:37 +0530

    base

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+const { BrowserWindow, app } = require('electron');
+
+const createMainWindows = () => {
+    const mainWindows = new BrowserWindow({
+        title: 'ImageShrink',
+        width: 500,
+        height: 600,
+    });
+}
+
+app.on('ready', createMainWindows);
